4f4af9868eddef9a8ad0669da2bc72bea91edc0b
Evandro <evandrobch@hotmail.com>
2024-09-17 11:53:20 -0300

    Rodando

--- src/ExerciciosSA2/ExercicioDois.java
+++ src/ExerciciosSA2/ExercicioDois.java
@@ -1,4 +1,31 @@
 package ExerciciosSA2;
 
+import java.util.Scanner;
+
 public class ExercicioDois {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] valores = new int[4];
+        int soma = 0;
+
+        // Leitura dos valores
+        for (int i = 0; i < 4; i++) {
+            System.out.print("Digite o valor " + (i + 1) + ": ");
+            valores[i] = scanner.nextInt();
+            soma += valores[i];
+        }
+
+        // Cálculo da média
+        double media = soma / 4.0;
+
+        // Exibição da média e se é positiva ou negativa
+        System.out.println("Média: " + media);
+        if (media > 0) {  // Verifica se é positiva ou negativa
+            System.out.println("Positiva");
+        } else {
+            System.out.println("Negativa");
+        }
+
+        scanner.close();
+    }
 }
